02ed5103b59090fa6e702f1ffb2f357a0cb110c7
BlueM0ld <blu3m0ld@gmail.com>
2024-07-03 15:01:00 +0100

    fixed graph implementation - original was incorrect

--- example/graph_plotting.py
+++ example/graph_plotting.py
@@ -3,14 +3,28 @@ import matplotlib.pyplot as plt
 from fpylll import IntegerMatrix, GSO
 
 
-def plot_gso(log_gso_norms, output_file="gso_plot.png"):
-    plt.figure(figsize=(10, 6))
-    for i, vec in enumerate(log_gso_norms):
-        plt.plot(range(len(vec)), vec, label=f"Vector {i+1}")
+def plot_gso(original_log_gso_norms, reduced_log_gso_norms, output_file="gso_plot.png"):
+    plt.figure(figsize=(14, 6))
+
+    # subplot 1 - M Orginal GSO norms
+    plt.subplot(1, 2, 1)
+    for i, vec in enumerate(original_log_gso_norms):
+        plt.plot(range(len(vec)), vec, label=f"Original Vector {i+1}")
     plt.ylabel("log Gram-Schmidt Norms")
-    plt.title("LLL Reduction")
+    plt.title("Original GSO Norms")
     plt.legend()
+
+    # subplot 2 - Reduced GSO norms
+    plt.subplot(1, 2, 2)
+    for i, vec in enumerate(reduced_log_gso_norms):
+        plt.plot(range(len(vec)), vec,
+                 label=f"Reduced Vector {i+1}", linestyle='--')
+    plt.ylabel("log Gram-Schmidt Norms")
+    plt.title("Reduced GSO Norms - LLL Reduction")
+    plt.legend()
+
     plt.savefig(output_file)
+    plt.show()
     plt.close()
 
 
@@ -21,17 +35,28 @@ def compute_and_plot_gso(M, output_file="gso_plot"):
     print("GSO norms", M_gso)
     M_gso.update_gso()
     square_gso_norms = M_gso.r()
-    log_gso_norms = [RR(log(square_gso_norm, 2)/2)
-                     for square_gso_norm in square_gso_norms]
-
-    plot_gso([log_gso_norms], "res/"+output_file+".png")
+    original_log_gso_norms = [RR(log(square_gso_norm, 2)/2)
+                              for square_gso_norm in square_gso_norms]
 
     reducedM = M.LLL()
+
+    reducedM_fpylll_matrix = convert_to_fpylll(reducedM)
+    print("fpylll matrix", reducedM_fpylll_matrix)
+    reducedM_gso = GSO.Mat(reducedM_fpylll_matrix)
+    print("GSO norms", reducedM_gso)
+    reducedM_gso.update_gso()
+    red_square_gso_norms = reducedM_gso.r()
+    reduced_log_gso_norms = [RR(log(red_square_gso_norm, 2)/2)
+                             for red_square_gso_norm in red_square_gso_norms]
+
+    plot_gso([original_log_gso_norms], [reduced_log_gso_norms],
+             "res/" + output_file + ".png")
+
     print("Reduced Matrix generated:")
-    print(M.str(rep_mapping=lambda x: str(x.n(digits=3))))
-    print("COL:", M.ncols())
-    print("ROWS:", M.nrows())
-    print("DIMENSIONS:", M.dimensions())
+    print(reducedM.str(rep_mapping=lambda x: str(x.n(digits=3))))
+    print("COL:", reducedM.ncols())
+    print("ROWS:", reducedM.nrows())
+    print("DIMENSIONS:", reducedM.dimensions())
 
     return reducedM
 
@@ -39,6 +64,7 @@ def compute_and_plot_gso(M, output_file="gso_plot"):
 def convert_to_fpylll(mat):
     # There's an issue with infinite values - this is arbitary as can't find a limit!
     max_val = 10**20
-    safe_mat = mat.apply_map(lambda x: min(max_val, x)
-                             if x != float('inf') else max_val)
+    min_val = -10**20
+    safe_mat = mat.apply_map(lambda x: min(max_val, max(min_val, x)) if x not in [
+                             float('inf'), float('-inf')] else max_val if x == float('inf') else min_val)
     return IntegerMatrix.from_matrix(safe_mat)
